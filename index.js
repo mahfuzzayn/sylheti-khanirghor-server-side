aedaafb06810f4cacc398f42e7829a3a0c8c4502
Mahfuz The Programmer Vetern <mahfuzzayn8@gmail.com>
2023-05-03 13:26:26 +0600

    Added cors to load data from server domain address

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const cors = require("cors");
 const chefs = require("./data/chefs.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Sylhet Khanirghor server running...");
