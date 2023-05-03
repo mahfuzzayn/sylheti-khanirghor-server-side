18ff81569863e155c2a2c50938f19993a1b25f40
Mahfuz The Programmer Vetern <mahfuzzayn8@gmail.com>
2023-05-03 13:22:15 +0600

    Added 6 Chefs data with recipes and all information in chefs.json file

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
+const chefs = require("./data/chefs.json");
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.send("Sylhet Khanirghor server running...");
-})
+});
+
+app.get("/chefs", (req, res) => {
+    res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chefs.find((chef) => chef._id === id);
+    res.send(selectedChef);
+});
 
 app.listen(port, () => {
     console.log(`Sylheti Khanirghor server is listening on port: ${port}`);
-})
+});
